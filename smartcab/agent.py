246e3406aac5a4becd131e27f2f7a7b341d4355f
ucaiado <ucaiado@yahoo.com.br>
2016-07-17 14:07:47 -0300

    feat: Start to implement a basic agent

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -16,7 +16,7 @@ import logging
 import sys
 import time
 
-# global variable
+# Log finle enabled. global variable
 DEBUG = True
 
 # setup logging messages
@@ -35,8 +35,82 @@ ch.setFormatter(formatter)
 root.addHandler(ch)
 
 
-class LearningAgent(Agent):
-    """An agent that learns to drive in the smartcab world."""
+class BasicAgent(Agent):
+    """
+    A Basic agent representation that learns to drive in the smartcab world.
+    """
+
+    def __init__(self, env):
+        # sets self.env = env, state = None, next_waypoint = None, and a
+        # default color
+        super(BasicAgent, self).__init__(env)
+        # override color
+        self.color = 'green'
+        # simple route planner to get next_waypoint
+        self.planner = RoutePlanner(self.env, self)
+        # TODO: Initialize any additional variables here
+
+
+
+    def reset(self, destination=None):
+        self.planner.route_to(destination)
+        # TODO: Prepare for a new trip; reset any variables here, if required
+        self.state = None
+        self.next_waypoint = None
+        self.l_rewards = []
+
+    def update(self, t):
+        # Gather inputs
+        # from route planner, also displayed by simulator
+        self.next_waypoint = self.planner.next_waypoint()
+        inputs = self.env.sense(self)
+        deadline = self.env.get_deadline(self)
+
+        # TODO: Update state
+        self.state = {'next_waypoint': self.next_waypoint,
+                      'inputs': inputs,
+                      'deadline': deadline}
+
+        # TODO: Select action according to your policy
+        action = None
+        action = self.next_waypoint
+        self.next_waypoint = self._take_action()
+
+        # Execute action and get reward
+        reward = self.env.act(self, action)
+
+        # TODO: Learn policy based on state, action, reward
+        self._apply_policy(self.state, action, reward)
+
+        # [debug]
+        s_rtn = 'LearningAgent.update(): deadline = {}, inputs = {}, action'
+        s_rtn += ' = {}, reward = {}'
+        if DEBUG:
+            root.debug(s_rtn.format(deadline, inputs, action, reward))
+        else:
+            print s_rtn.format(deadline, inputs, action, reward)
+
+    def _take_action(self):
+        '''
+        Return an action according to the agent policy
+        '''
+        return random.choice(Environment.valid_actions)
+
+
+    def _apply_policy(self, state, action, reward):
+        '''
+        Learn policy based on state, action, reward
+        :param state: dictionary. The current state of the agent
+        :param action: string. the action selected at this time
+        :param reward: integer. the rewards received due to the action
+        '''
+        pass
+
+
+class LearningAgent(BasicAgent):
+    """
+    An agent that learns to drive in the smartcab world.
+    """
 
     def __init__(self, env):
         # sets self.env = env, state = None, next_waypoint = None, and a
@@ -77,19 +151,36 @@ class LearningAgent(Agent):
         else:
             print s_rtn.format(deadline, inputs, action, reward)
 
+    def _take_action(self):
+        '''
+        Return an action according to the agent policy
+        '''
+        return random.choice(Environment.valid_actions)
+
+
+    def _apply_policy(self, state, action, reward):
+        '''
+        Learn policy based on state, action, reward
+        :param state: dictionary. The current state of the agent
+        :param action: string. the action selected at this time
+        :param reward: integer. the rewards received due to the action
+        '''
+        pass
+
 
 def run():
     """Run the agent for a finite number of trials."""
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  # create agent
+    a = e.create_agent(BasicAgent)  # create agent
+    # a = e.create_agent(LearningAgent)  # create agent
     e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow
     # longer trials
 
     # Now simulate it
     # create simulator (uses pygame when display=True, if available)
-    sim = Simulator(e, update_delay=0.5, display=False)
+    sim = Simulator(e, update_delay=0.5, display=True)
     # NOTE: To speed up simulation,reduce update_delay and/or set display=False
 
     sim.run(n_trials=2)  # run for a specified number of trials
